3d941979278e46da9cf57610c923c5df87b9bdc8
PN <priteshrna@gmail.com>
2021-11-10 22:03:30 -0500

    function added

--- CollectionViewMenuProject/ViewController.swift
+++ CollectionViewMenuProject/ViewController.swift
@@ -9,13 +9,16 @@ import UIKit
 
 class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
     
-    let iamgeNames = Array(1...6).map { "image\($0)" }
+    let imageNames = Array(1...6).map { "image\($0)" }
     
     private let collectionView: UICollectionView = {
         let layout = UICollectionViewFlowLayout()
         layout.scrollDirection = .vertical
+        layout.minimumLineSpacing = 2
+        layout.minimumInteritemSpacing = 2
         let collectionView = UICollectionView(frame: .zero,
                                               collectionViewLayout: layout)
+        collectionView.backgroundColor = .systemBackground
         collectionView.register(ImageCollectionViewCell.self, forCellWithReuseIdentifier: ImageCollectionViewCell.identifier)
         return collectionView
     }()
@@ -34,18 +37,32 @@ class ViewController: UIViewController, UICollectionViewDataSource, UICollection
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 0
+        return imageNames.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: ImageCollectionViewCell.identifier, for: indexPath) as? ImageCollectionViewCell else {
             fatalError()
         }
-        
+        cell.imageView.image = UIImage(named: imageNames[indexPath.row])
         return cell
     }
     
-    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: (view.frame.size.width/2)-4, height: (view.frame.size.width/2)-4)
+    }
+    // contect menu function here!!
+    func collectionView(_ collectionView: UICollectionView, contextMenuConfigurationForItemAt indexPath: IndexPath, point: CGPoint) -> UIContextMenuConfiguration? {
+        let config = UIContextMenuConfiguration(identifier: nil,
+                                                previewProvider: nil) { _ in
+            return UIMenu(title: "",
+                          image: nil,
+                          identifier: nil,
+                          options: UIMenu.Options.displayInline,
+                          children: [])
+        }
+        return config
+    }
 }
 
 class ImageCollectionViewCell : UICollectionViewCell {
@@ -53,6 +70,7 @@ class ImageCollectionViewCell : UICollectionViewCell {
     
     let imageView: UIImageView = {
        let imageView = UIImageView()
+        imageView.clipsToBounds = true
         imageView.contentMode = .scaleAspectFill
         return imageView
     }()
